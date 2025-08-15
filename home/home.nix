e42b0fd4d6ed3d8ecae7f722240bbe64fdba86b2
rb60 <rmngncua@gmail.com>
2025-08-15 14:50:15 +0300

    bash aliases for rebuild added

--- home/home.nix
+++ home/home.nix
@@ -9,7 +9,8 @@
 	programs.bash = {
 		enable = true;
 		shellAliases = {
-			rebuild = "sudo nixos-rebuild switch";
+			rebuild = "sudo nixos-rebuild switch --flake /home/tima/nix/";
+			rebuild-home = "home-manager switch --flake /home/time/nix";
 		};
 
 	};
