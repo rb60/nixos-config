d1440c437980bc05362c7eb9e2be442e15b855bb
rb60 <rmngncua@gmail.com>
2025-08-15 14:09:46 +0300

    keyboard layout added

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,9 +60,14 @@
   # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkb.layout = "us,ua";
+    xkbVariant = ",";
+    xkbOptions = "grp:win_space_toggle";
+  };
 
   
 
