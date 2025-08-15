d4008d0fbee904be9f60cc637f7348302816959d
rb60 <rmngncua@gmail.com>
2025-08-15 23:40:55 +0300

    added telegram and steam

--- home/home.nix
+++ home/home.nix
@@ -4,13 +4,14 @@
 		username = "tima";
 		homeDirectory = "/home/tima";
 		stateVersion = "25.05";
+		packages = with pkgs[ telegram-desktop ]
 	};
 
 	programs.bash = {
 		enable = true;
 		shellAliases = {
 			rebuild = "sudo nixos-rebuild switch --flake /home/tima/nix/";
-			rebuild-home = "home-manager switch --flake /home/time/nix";
+			rebuild-home = "home-manager switch --flake /home/time/nix/";
 		};
 
 	};
@@ -18,6 +19,7 @@
 	imports = [
 		./modules/git.nix
 		./modules/github.nix
+		./modules/steam.nix
 	];
 	
 	dconf.settings."org/gnome/desktop/wm/preferences".button-layout = "minimize,maximize,close";
