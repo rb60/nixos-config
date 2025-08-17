11ce7b7d8a3c0ee6de4fc30ffd863eb630525312
rb60 <rmngncua@gmail.com>
2025-08-17 23:22:16 +0300

    added config for terminal font

--- home/home.nix
+++ home/home.nix
@@ -23,6 +23,12 @@
 	];
 	
 	dconf.settings."org/gnome/desktop/wm/preferences".button-layout = "minimize,maximize,close";
+	dconf.settings."org/gnome/Console" = 
+	{
+		custom-font = "Agave Nerd Font 10";
+		font-scale = 1.2;
+		use-system-font = false;
 
+	};
 	
 }
