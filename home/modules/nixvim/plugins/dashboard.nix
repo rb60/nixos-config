8b14ca22cb8fdb15773ff44250c67ea8a381381f
rb60 <rmngncua@gmail.com>
2025-08-18 16:44:05 +0300

    nixvim added stings to dashboard

--- home/modules/nixvim/plugins/dashboard.nix
+++ home/modules/nixvim/plugins/dashboard.nix
@@ -2,5 +2,35 @@
 	programs.nixvim.plugins.dashboard = 
 	{
 		enable = true;
+		settings = {
+  			change_to_vcs_root = true;
+  			config = {
+    				footer = [
+      					"Made with ❤️"
+    				];
+    			header = [
+      				"███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+      				"████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+      				"██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+      				"██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+      				"██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+      				"╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+    				];
+    			mru = {
+      				limit = 20;
+    			};
+    			project = {
+      				enable = false;
+    			};
+    			shortcut = [
+      			
+    			];
+    			week_header = {
+      				enable = true;
+    			};
+  		};
+  		theme = "hyper";
+		};
+		
 	};
 }
