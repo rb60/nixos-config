6a36b0f160d94fdb5dbd8300a5287b32454c9b79
rb60 <rmngncua@gmail.com>
2025-09-03 20:15:28 +0300

    cmp config changed

--- home/modules/nixvim/plugins/cmp.nix
+++ home/modules/nixvim/plugins/cmp.nix
@@ -2,8 +2,18 @@
 	programs.nixvim.plugins.cmp = 
 	{
 		enable = true;
-		settings.sources = [
-			{ name = "nvim_lsp";}
+		settings = {
+			sources = [
+				{ name = "nvim_lsp";}
 			];
+			completion =
+			{
+				autocomplete = 
+					[
+						"require('cmp.types').cmp.TriggerEvent.TextChanged"
+					];
+				completeopt = "menu,menuone,noselect";
+			};		
+		};
 	};
 }
