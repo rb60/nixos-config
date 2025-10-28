907c11d5d865a363d3a23b99d3a2b71112a0dbe8
rb60 <rmngncua@gmail.com>
2025-10-28 17:09:08 +0200

    wayland disabled

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,6 +71,7 @@
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = false;
     desktopManager.gnome.enable = true;
     xkb.layout = "us,ua";
     xkb.options = "grp:win_space_toggle";
