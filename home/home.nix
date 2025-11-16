e8704cf640460fd6a4e8a23de3358624efa24e60
rb60 <rmngncua@gmail.com>
2025-11-16 21:27:52 +0200

    some changes

--- home/home.nix
+++ home/home.nix
@@ -30,7 +30,7 @@
 
 		Service = {
 			ExecStartPre = pkgs.writeShellScript "wait-for-mount" ''
-			until [-d "/run/media/tima/Elements"]
+			until [-f "/run/media/tima/Elements"]
 			do
 				ls -l /run/media/tima/ 2>&1 || echo "Path /run/media/tima/ not found"
 				sleep 2
